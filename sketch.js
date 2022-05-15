5b136f470fe45ce8cba3d5d00e980be6b875eca8
surayya-lab <76586836+surayya-lab@users.noreply.github.com>
2022-05-16 01:20:10 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var bg, bgImg
+var bottomGround
+var topGround
+var balloon, balloonImg
+
+function preload(){
+
+}
+
+function setup(){
+
+}
+
+function draw() {
+  
+  background("black");
+
+        
+}
